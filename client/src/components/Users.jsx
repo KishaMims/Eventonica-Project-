ac42fb9e1319170565b0efef76658677c6e0f913
KishaMims <86165064+KishaMims@users.noreply.github.com>
2022-03-17 16:26:10 -0500

    post working

--- client/src/components/Users.jsx
+++ client/src/components/Users.jsx
@@ -1,6 +1,7 @@
 //import res from 'express/lib/response';
 import React from 'react';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+//import { response } from '../../../server/app';
 import DeleteUser from './DeleteUser';
 // import { Buffer } from 'buffer';
 // global.Buffer = Buffer;
@@ -15,40 +16,74 @@ export default function Users() {
     const handleDeleteUser = (deleteId) => {
         const newUsers = users.filter((i) => i.id !== deleteId);
         setUsers(newUsers);
-      };
-    
+    };
+
 
     const handleSubmit = (e) => {
-    e.preventDefault();
-    const newUser = { id: id, name: name, email: email };
-    setUsers([...users, newUser]);
-  };
+        e.preventDefault();
+        const newUser = { id: id, name: name, email: email };
+        addUsers(newUser);
+        setUsers([...users, newUser]);
+    };
 
-  //marlin, nemo, dory]
+    //marlin, nemo, dory]
     const [users, setUsers] = useState([]);
     const [name, setName] = useState(" ");
     const [email, setEmail] = useState(" ");
     const [id, setID] = useState(" ");
-    
-    // const getUsers = () => {
-    //     fetch(`http://localhost:4000/users`)
-    //       .then((res) => res.json())
-    //       .then((res) => setUsers(res.users));
-    //   };
-      
-    //   useEffect(() => {
-    //     // useEffect will run getUsers() every time this component loads, as opposed to just the first time it is rendered.
-    //     getUsers();
-    //   }, []);
 
-    // useEffect(() => {
+    const getUsers = () => {
+        fetch(`http://localhost:4000/users`)
+            .then((res) => res.json())
+            .then((res) => setUsers(res.users));
+    };
+
+
+    useEffect(() => {
+        // useEffect will run getUsers() every time this component loads, as opposed to just the first time it is rendered.
+        getUsers();
+    }, []);
+
+    // POST request using fetch inside useEffect React hook
+    //     const addUsers = () => {
+    //         method: 'POST',
+    //         body: JSON.stringify(setUsers)
+    //     },[])
     //     fetch(`http://localhost:4000/users`)
-    //         .then((response) => response.json())
-    //         .then(info => {
-    //             setUsers(info.results);
-    //             console.log('Users fetched...', info);
-    //         })
+    //         .then(response => response.json())
+    //         .then((res) => setUsers(res.users));
+    //         console.log('new user added');
+
+    //   useEffect(() => {
+    //     addUsers();
+    //   }, []);
+    const addUsers = (newUser) => {
+        fetch(`http://localhost:4000/users`, {
+            method: 'POST',
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(newUser)
+    }).then(() => {
+            console.log('new user added');
+        })
+    };
+
+    useEffect(() => {
+            
+          }, []);
+    // useEffect(() => {
+    //     // POST request using fetch inside useEffect React hook
+    //     const addUsers = {
+    //         method: 'POST',
+    //         body: JSON.stringify(setUsers)
+    //     };
+    //     fetch('http://localhost:4000/users', addUsers)
+    //         .then((res) => res.json())
+    //         .then((res) => setUsers(res.users));
+
+    //     // empty dependency array means this effect will only run once (like componentDidMount in classes)
     // }, []);
+
+
 
     return (
         <section className="user-management">
@@ -65,37 +100,37 @@ export default function Users() {
                 <form id="add-user" onSubmit={handleSubmit}>
                     <fieldset>
                         <label>Name</label>
-                        <input 
-                        type="text" 
-                        placeholder="Enter Name"
-                        id="add-user-name"
-                        value={name}
-                        onChange={(e) =>setName(e.target.value)}
+                        <input
+                            type="text"
+                            placeholder="Enter Name"
+                            id="add-user-name"
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
                         />
-                    
-                    {/* Add more form fields here */}
-                    <label>Email</label>
-                    <input 
-                    type="text"
-                    id="add-user-email"
-                    placeholder="Enter Email"
-                    value={email} 
-                    onChange={(e) =>setEmail(e.target.value)}
-                    />
-                    <label>Id:</label>
-                    <input
-                    type="text"
-                    id="add-user-id"
-                    placeholder="ID"
-                    value={id}
-                    onChange={(e) => setID(e.target.value)}
-                    />
+
+                        {/* Add more form fields here */}
+                        <label>Email</label>
+                        <input
+                            type="text"
+                            id="add-user-email"
+                            placeholder="Enter Email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                        <label>Id:</label>
+                        <input
+                            type="text"
+                            id="add-user-id"
+                            placeholder="ID"
+                            value={id}
+                            onChange={(e) => setID(e.target.value)}
+                        />
                     </fieldset>
                     <button type="submit">Add</button>
                 </form>
             </div>
             <div>
-            <DeleteUser handleDeleteUser={handleDeleteUser}/>
+                <DeleteUser handleDeleteUser={handleDeleteUser} />
             </div>
         </section>
     )
--- server/routes/users.js
+++ server/routes/users.js
@@ -14,4 +14,32 @@ router.get('/', function(req, res, next) {
   res.json({ users: mockUsers });
 });
 
+// router.post('/users', (req, res) => {
+//   console.log('got users:', req.body);
+//   res.sendStatus(200);
+// });
+
+// router.post('/', (req, res) => {
+//   res.json({requestBody: req.body})  // <==== req.body will be a parsed JSON object
+// })
+
+router.post('/', function (req, res, next) {
+  // save request data to a variable in routes/users.js
+mockUsers.push(req.body);
+res.json({requestBody: req.body});
+res.status(201).send(`Added user ${res.body}`);
+res.send(req.body);
+});
+
+
+
+// app.post('/book', (req, res) => {
+//   const book = req.body;
+
+//   console.log(book);
+//   books.push(book);
+//   res.send('Book is added to the database');
+
+// });
+
 module.exports = router;
